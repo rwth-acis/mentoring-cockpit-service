0a97dd21e2a0b307356246cd0a71473de5fc01ef
Michi <mic.michael@gmx.de>
2020-06-18 22:56:42 +0200

    add kubernetes pipeline

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+CA_CERT=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+curl -k -X PUT --cacert $CA_CERT -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d @mentoring-cockpit-service.json "https://137.226.232.175:6443/apis/apps/v1/namespaces/$KUBERNETES_NAMESPACE/deployments/workbench"
+POD_NAME=$(curl -k --cacert $CA_CERT -H "Authorization: Bearer $TOKEN" "https://137.226.232.175:6443/api/v1/namespaces/$KUBERNETES_NAMESPACE/pods" | jq '.items[] | .metadata.name' | grep mentoring-cockpit-service | sed 's/"//g')
+curl -k -X DELETE --cacert $CA_CERT -H "Authorization: Bearer $TOKEN" "https://137.226.232.175:6443/api/v1/namespaces/$KUBERNETES_NAMESPACE/pods/$POD_NAME"
